bbf1d6df0c57b5737a38dbb5a4664125e714a3d8
Himanshu-learner <himanshuchaurasiya1818@gmail.com>
2024-01-29 22:12:55 +0530

    .

--- Login.js
+++ Login.js
@@ -12,3 +12,48 @@ login.addEventListener("click", () => {
 	slider.classList.remove("moveslider");
 	formSection.classList.remove("form-section-move");
 });
+
+
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+	var $this = $(this),
+		label = $this.prev('label');
+  
+		if (e.type === 'keyup') {
+			  if ($this.val() === '') {
+			label.removeClass('active highlight');
+		  } else {
+			label.addClass('active highlight');
+		  }
+	  } else if (e.type === 'blur') {
+		  if( $this.val() === '' ) {
+			  label.removeClass('active highlight'); 
+			  } else {
+			  label.removeClass('highlight');   
+			  }   
+	  } else if (e.type === 'focus') {
+		
+		if( $this.val() === '' ) {
+			  label.removeClass('highlight'); 
+			  } 
+		else if( $this.val() !== '' ) {
+			  label.addClass('highlight');
+			  }
+	  }
+  
+  });
+  
+  $('.tab a').on('click', function (e) {
+	
+	e.preventDefault();
+	
+	$(this).parent().addClass('active');
+	$(this).parent().siblings().removeClass('active');
+	
+	target = $(this).attr('href');
+  
+	$('.tab-content > div').not(target).hide();
+	
+	$(target).fadeIn(600);
+	
+  });
